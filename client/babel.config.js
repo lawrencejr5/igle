d67a2eecbfcc94cc9913d5e219b6c0970f2b52d4
lawrencejr5 <oputalawrence@gmail.com>
2026-08-18 23:00:24 +0100

    fix: add expo-asset dependency

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,7 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"], // 👈 keeps Expo defaults
-    plugins: [],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "expo-asset",
+      "react-native-reanimated/plugin", // 👈 Must be the LAST plugin
+    ],
   };
 };
